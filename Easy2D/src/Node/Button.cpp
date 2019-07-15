fd93c9430cc1ca4d4028620dcb22a812049ffe57
Nomango <569629550@qq.com>
2019-07-16 06:59:42 +0800

    去除调试代码

--- Easy2D/src/Node/Button.cpp
+++ Easy2D/src/Node/Button.cpp
@@ -217,11 +217,6 @@ void easy2d::Button::onUpdate()
 		}
 		else if (_normal->getBoundingBox().containsPoint(Input::getMousePos()))
 		{
-			auto box = _normal->getBoundingBox();
-			E2D_LOG(L"IMAGE %.1f %.1f %.1f %.1f (%.1f %.1f)"
-				, box.getLeft(), box.getRight(), box.getTop(), box.getBottom()
-				, Input::getMousePos().x, Input::getMousePos().y);
-
 			_setState(ButtonState::Mouseover);
 			Window::setCursor(Window::Cursor::Hand);
 			return;
